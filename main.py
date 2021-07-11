a25ebd8223556ff12630fbb3e8b06bb57da4c7c3
Milana <milanasirozhova@yandex.ru>
2021-07-11 19:17:34 +0500

    Добавлен порт для heroku

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from data import db_session
 import api
+import os
 
 app = Flask(__name__)
 
@@ -8,7 +9,8 @@ app = Flask(__name__)
 def main():
     db_session.global_init("db/db_products.db")
     app.register_blueprint(api.blueprint)
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port=port)
 
 
 if __name__ == '__main__':
